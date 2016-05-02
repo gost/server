943ccc171a553032bdcbe74b012bf5ff0522ceb7
bertt <btemme@hotmail.com>
2016-05-02 19:12:46 +0200

    add things post

--- client/resources/js/main.js
+++ client/resources/js/main.js
@@ -32,22 +32,44 @@ function getThings(){
     });
 }
 
+function showThing(thing){
+    var html="Id: " + thing["@iot.id"] + "<br/>"; 
+    html+="Description: " + thing.description + "<br/>";
+    html+="Organisation: " + thing.properties.organisation + "<br/>";
+    html+="Owner: " + thing.properties.owner + "<br/>";
+    html += "Link: " + "<a href = '" + thing["@iot.selfLink"] + "'>" + thing["@iot.selfLink"] + "</a><br/>";
+    html += "<br/>";
+    return html;
+}
+
 function showThings(data){
     var html = "Number of things: " +  data.count + "<br/><br/>";
     
     for(var i=0;i<data.count;i++){
         html += "<b>thing " + i + "</b><br/>";
         var val0=data.value[i];
-        html+="Id: " + val0["@iot.id"] + "<br/>"; 
-        html+="Description: " + val0.description + "<br/>";
-        html+="Organisation: " + val0.properties.organisation + "<br/>";
-        html+="Owner: " + val0.properties.owner + "<br/>";
-        html += "Link: " + "<a href = '" + val0["@iot.selfLink"] + "'>" + val0["@iot.selfLink"] + "</a><br/>";
-        html += "<br/>";
+        html+=showThing(val0);
     }
     
     $('#content').html(html);
 }
+
+function postThing(){
+   var body= {"description": "Testsensor1","properties": {"organisation": "Geodan","owner": "Bert"}};
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify(body),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        url: getUrl() + "/v1.0/Things",
+        success: function(data){
+            var html = "Thing created! <br/><br/>";
+            html += showThing(data);
+            $('#content').html(html);
+        }
+    });
+}
+
 
 function getUrl(){
     return window.location.origin;
