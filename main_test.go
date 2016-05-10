46fad17267f7ee14a76e4d9ea9a99b6ca3978321
bertt <bert@berts-MacBook-Pro.fritz.box>
2016-05-10 08:50:28 +0200

    add package main tests

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+    // todo add startup tests...
+    // arrange
+    var a=1
+    var b=2
+    // act
+    var res = a+b
+    // assert
+	assert.Equal(t,3, res, "computer error again")
+}
