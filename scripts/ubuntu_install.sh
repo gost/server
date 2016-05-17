3246d156caa95abc6eae4d3653d117a634943608
Bert Temme <btemme@hotmail.com>
2016-05-17 15:46:59 +0200

    Update ubuntu_install.sh

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -1,7 +1,7 @@
 #-------------------------
 # update system and tools
 #-------------------------
-sudo apt -y update
+sudo apt-get -y update
 sudo apt-get -y install git
 sudo apt-get -y install mosquitto
 
