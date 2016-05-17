7c21f4ca82a4657f029f6ca1265d5ff3cbe9e56b
tebben <ebben.tim@gmail.com>
2016-05-17 09:20:11 +0200

    test

--- src/sensorthings/mqtt/config_test.go
+++ src/sensorthings/mqtt/config_test.go
@@ -1,1 +1,17 @@
 package mqtt
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestModels(t *testing.T) {
+	// todo add models tests...
+	// arrange
+	var a = 1
+	var b = 2
+	// act
+	var res = a + b
+	// assert
+	assert.Equal(t, 3, res, "computer error again")
+}
