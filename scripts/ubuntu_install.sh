7407de02f1e3a5d93e32c8a61a70d627cb0a6b09
bertt <btemme@hotmail.com>
2016-05-18 16:02:17 +0200

    install script use default config

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -35,44 +35,8 @@ EOF
 #-------------------------
 cd ~/dev/go/src/github.com/geodan
 git clone https://github.com/Geodan/gost.git
-go get github.com/gorilla/mux
-go get gopkg.in/yaml.v2
-go get github.com/lib/pq
-go get github.com/eclipse/paho.mqtt.golang
 
-#Build GOST to bin folder
-go build -o gost/bin/gost github.com/geodan/gost/src
-
-#Copy the createdb script to bin folder
-sudo cp ~/dev/go/src/github.com/geodan/gost/scripts/createdb.sql ~/dev/go/src/github.com/geodan/gost/bin
-
-#Copy the client folder to bin
-sudo cp -avr ~/dev/go/src/github.com/geodan/gost/src/client ~/dev/go/src/github.com/geodan/gost/bin
-
-#Write a config.yaml
-echo "server:
-    name: GOST Server
-    host: 0.0.0.0
-    port: 8080
-    externalUri: http://localhost:8080/
-database:
-    host: localhost
-    port: 5432
-    user: postgres
-    password: postgres
-    database: gost
-    schema: v1
-    ssl: false
-mqtt:
-    enabled: true
-    host: 127.0.0.1
-    port: 1883" > ~/dev/go/src/github.com/geodan/gost/bin/config.yaml
-
-#Setup database schema for GOST
-~/dev/go/src/github.com/geodan/gost/bin/gost -install ~/dev/go/src/github.com/geodan/gost/bin/createdb.sql -config ~/dev/go/src/github.com/geodan/gost/bin/config.yaml
 
 #Open port
 sudo iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -j ACCEPT -m comment --comment "GOST Server port"
 
-#Run GOST
-~/dev/go/src/github.com/geodan/gost/bin/gost -config ~/dev/go/src/github.com/geodan/gost/bin/config.yaml
