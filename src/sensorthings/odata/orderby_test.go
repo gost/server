a1c9fbcce443197c4ea243d3a19a9fabb328907b
bert <btemme@hotmai.com>
2016-05-19 09:18:11 +0200

    add odata orderby test

---
+++ src/sensorthings/odata/orderby_test.go
@@ -0,0 +1,43 @@
+package odata
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOrderByWithEmptyOrderShouldReturnError(t *testing.T) {
+	//arrange
+	orderby := QueryOrderBy{}
+
+	//act
+	err := orderby.Parse("")
+
+	//assert
+	assert.NotNil(t, err, "Empty QueryOrderBy should give errors")
+}
+
+func TestOrderByParsingShouldGivePropertyAndSuffix(t *testing.T) {
+	//arrange
+	orderby := QueryOrderBy{}
+
+	//act
+	err := orderby.Parse("hallo asc")
+
+	//assert
+	assert.Nil(t, err, "Empty QueryOrderBy should give errors")
+	assert.Equal(t, orderby.property, "hallo", "orderby.property should be correct")
+	assert.Equal(t, orderby.suffix, "asc", "orderby.suffix should be correct")
+ }
+
+// todo: add test for IQueryOrderBy.IsValid when implementing
+
+func TestOrderByNotShouldReturnNotNil(t *testing.T) {
+	//arrange
+	orderby := QueryOrderBy{}
+
+	//act
+	res := orderby.IsNil()
+
+	//assert
+	assert.False(t, res, "OrderBy should not be nil")
+ }
