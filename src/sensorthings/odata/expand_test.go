772c2b072e21847f8d5813540cb934ffabde5b2e
bert <btemme@hotmai.com>
2016-05-19 09:49:22 +0200

    add expand test

---
+++ src/sensorthings/odata/expand_test.go
@@ -0,0 +1,58 @@
+package odata
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExpandParserShouldFillParams(t *testing.T) {
+	//arrange
+	expand := QueryExpand{}
+
+	//act
+	res := expand.Parse("fld1,fld2")
+    
+
+	//assert
+	assert.Nil(t, res, "Parse result should be nil")
+	assert.True(t, len(expand.params)>0, "Expand params should be filled")
+}
+
+func TestExpandIsValidShouldReturnTrueWhenValid(t *testing.T) {
+	//arrange
+	expand := QueryExpand{}
+	expand.Parse("fld1,fld2")
+    var vals = []string{"fld1","fld2"}
+
+	//act
+    bln, _ := expand.IsValid(vals,"haha")
+
+	//assert
+	assert.True(t, bln, "Expand IsValid should return correct value")
+}
+
+func TestExpandIsValidShouldReturnFalseWhenNotValid(t *testing.T) {
+	//arrange
+	expand := QueryExpand{}
+	expand.Parse("fld1,fld2")
+    var vals = []string{"hoho","haha"}
+
+	//act
+    bln, _ := expand.IsValid(vals,"haha")
+
+	//assert
+	assert.False(t, bln, "Expand IsValid should return correct value")
+}
+
+
+func TestExpandisNilNotShouldReturnNotNil(t *testing.T) {
+	//arrange
+	expand := QueryOrderBy{}
+
+	//act
+	res := expand.IsNil()
+
+	//assert
+	assert.False(t, res, "Expand should not be nil")
+ }
+
