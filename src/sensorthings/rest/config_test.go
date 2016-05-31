d8630e142a7ba55dcfa1ad69a178662528c3ecd1
bertt <btemme@hotmail.com>
2016-05-31 16:51:31 +0200

    fix test

--- src/sensorthings/rest/config_test.go
+++ src/sensorthings/rest/config_test.go
@@ -12,7 +12,7 @@ func TestCreateEndPoints(t *testing.T) {
 	endpoints := CreateEndPoints("http://test.com")
 
 	//assert
-	assert.Equal(t, len(endpoints), 10)
+	assert.Equal(t, len(endpoints), 9)
 }
 
 func TestCreateEndPointVersion(t *testing.T) {
