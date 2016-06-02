305ca1f6709be3679e9efd8bb7eb78fdae30416a
bertt <btemme@hotmail.com>
2016-06-02 11:48:05 +0200

    crlf -> lf

--- src/http/router_test.go
+++ src/http/router_test.go
@@ -1,22 +1,22 @@
 package http
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
-	"github.com/geodan/gost/src/sensorthings/api"
+	"github.com/geodan/gost/src/configuration"
 	"github.com/geodan/gost/src/database/postgis"
 	"github.com/geodan/gost/src/mqtt"
-	"github.com/geodan/gost/src/configuration"
+	"github.com/geodan/gost/src/sensorthings/api"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-// TestHttp starts
+// Test the router functionality
 func TestCreateRouter(t *testing.T) {
 	// arrange
 	cfg := configuration.Config{}
 	mqttServer := mqtt.CreateMQTTClient(configuration.MQTTConfig{})
 	database := postgis.NewDatabase("", 123, "", "", "", "", false, 50, 100)
 	a := api.NewAPI(database, cfg, mqttServer)
-	
+
 	// act
 	router := CreateRouter(&a)
 
--- src/sensorthings/entities/observationtypes_test.go
+++ src/sensorthings/entities/observationtypes_test.go
@@ -5,20 +5,19 @@ import (
 	"testing"
 )
 
-
 func TestObservationTypesShouldReturnArray(t *testing.T) {
 	// assert
-	assert.True(t,len(ObservationTypes)>0, "Array of Observationtypes should be returned")
+	assert.True(t, len(ObservationTypes) > 0, "Array of Observationtypes should be returned")
 }
 
-func TestGetObservationTypeByValueShouldReturnCorrectObservationType(t *testing.T){
+func TestGetObservationTypeByValueShouldReturnCorrectObservationType(t *testing.T) {
 	// arrange
 	input := "http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CategoryObservation"
 
 	// act
-	res, err  := GetObservationTypeByValue(input)
+	res, err := GetObservationTypeByValue(input)
 
 	// assert
-	assert.Nil(t, err , "Error should not be nil")
+	assert.Nil(t, err, "Error should not be nil")
 	assert.True(t, res.Code == 0, "Code should be 0")
 }
