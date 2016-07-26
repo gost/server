7e44d867dd15e9b61dadc7308eb66d29fc618e0c
bert <btemme@hotmail.com>
2016-07-26 17:07:56 +0200

    more splitting up file 'handlers'

---
+++ src/sensorthings/rest/HttpObservationHandler.go
@@ -0,0 +1,74 @@
+package rest
+
+import (
+	"net/http"
+
+	"github.com/geodan/gost/src/sensorthings/entities"
+	"github.com/geodan/gost/src/sensorthings/models"
+	"github.com/geodan/gost/src/sensorthings/odata"
+)
+
+// HandleGetObservations ...
+func HandleGetObservations(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	handle := func(q *odata.QueryOptions, path string) (interface{}, error) { return a.GetObservations(q, path) }
+	handleGetRequest(w, endpoint, r, &handle)
+}
+
+// HandleGetObservation ...
+func HandleGetObservation(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	handle := func(q *odata.QueryOptions, path string) (interface{}, error) {
+		return a.GetObservation(getEntityID(r), q, path)
+	}
+	handleGetRequest(w, endpoint, r, &handle)
+}
+
+// HandleGetObservationsByFeatureOfInterest ...
+func HandleGetObservationsByFeatureOfInterest(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	handle := func(q *odata.QueryOptions, path string) (interface{}, error) {
+		return a.GetObservationsByFeatureOfInterest(getEntityID(r), q, path)
+	}
+	handleGetRequest(w, endpoint, r, &handle)
+}
+
+// HandleGetObservationsByDatastream ...
+func HandleGetObservationsByDatastream(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	handle := func(q *odata.QueryOptions, path string) (interface{}, error) {
+		return a.GetObservationsByDatastream(getEntityID(r), q, path)
+	}
+	handleGetRequest(w, endpoint, r, &handle)
+}
+
+// HandlePostObservation ...
+func HandlePostObservation(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	ob := &entities.Observation{}
+	handle := func() (interface{}, []error) { return a.PostObservation(ob) }
+	handlePostRequest(w, endpoint, r, ob, &handle)
+}
+
+// HandlePostObservationByDatastream ...
+func HandlePostObservationByDatastream(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	ob := &entities.Observation{}
+	handle := func() (interface{}, []error) { return a.PostObservationByDatastream(getEntityID(r), ob) }
+	handlePostRequest(w, endpoint, r, ob, &handle)
+}
+
+// HandleDeleteObservation ...
+func HandleDeleteObservation(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	handle := func() error { return a.DeleteObservation(getEntityID(r)) }
+	handleDeleteRequest(w, endpoint, r, &handle)
+}
+
+// HandlePatchObservation ...
+func HandlePatchObservation(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
+	a := *api
+	ob := &entities.Observation{}
+	handle := func() (interface{}, error) { return a.PatchObservation(getEntityID(r), ob) }
+	handlePatchRequest(w, endpoint, r, ob, &handle)
+}
