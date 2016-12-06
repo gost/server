4afff5669d09ca8d7762fba39c9a10ed1bb188f4
bert <btemme@hotmail.com>
2016-12-06 16:20:23 +0100

    create release only for mac, windows, linux 64 bit system and copy config/client also

--- scripts/xcompile.bat
+++ scripts/xcompile.bat
@@ -1,42 +1,27 @@
 @echo off
 
 if exist ..\bin rd /s /q ..\bin
-mkdir ..\bin\win32
 mkdir ..\bin\win64
-mkdir ..\bin\linux32
 mkdir ..\bin\linux64
-mkdir ..\bin\linuxARMv6
-mkdir ..\bin\linuxARMv7
-mkdir ..\bin\darwin32
 mkdir ..\bin\darwin64
 
 SET GOOS=windows
-SET GOARCH=386
-go build -o ..\bin\win32\gost.exe ../src/main.go
-echo "Built application for Windows/386"
 SET GOARCH=amd64
 go build -o ..\bin\win64\gost.exe ../src/main.go
 echo "Built application for Windows/amd64"
+copy ..\src\config.yaml ..\bin\win64\config.yaml
+xcopy ..\src\client\*.* ..\bin\win64\client\ /S
 
 SET GOOS=linux
-SET GOARCH=386
-go build -o ..\bin\linux32\gost ../src/main.go
-echo "Built application for Linux/386"
 SET GOARCH=amd64
 go build -o ..\bin\linux64\gost ../src/main.go
 echo "Built application for Linux/amd64"
-SET GOARCH=arm
-SET GOARM=6
-go build -o ..\bin\linuxARMv6\gost ../src/main.go
-echo "Built application for Linux/ARMv6"
-SET GOARM=7
-go build -o ..\bin\linuxARMv7\gost ../src/main.go
-echo "Built application for Linux/ARMv7"
+copy ..\src\config.yaml ..\bin\linux64\config.yaml
+xcopy ..\src\client\*.* ..\bin\linux64\client\ /S
 
 SET GOOS=darwin
-SET GOARCH=386
-go build -o ..\bin\darwin32\gost ../src/main.go
-echo "Built application for Darwin/386"
 SET GOARCH=amd64
 go build -o ..\bin\darwin64\gost ../src/main.go
 echo "Built application for Darwin/amd64"
+copy ..\src\config.yaml ..\bin\darwin64\config.yaml
+xcopy ..\src\client\*.* ..\bin\darwin64\client\ /S
