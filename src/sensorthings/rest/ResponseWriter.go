8f880584eb87d2595f93987bfdf0ef9b9a794aaa
Bert Temme <btemme@hotmail.com>
2017-04-26 13:45:58 +0200

    Update ResponseWriter.go

--- src/sensorthings/rest/ResponseWriter.go
+++ src/sensorthings/rest/ResponseWriter.go
@@ -65,6 +65,8 @@ func JSONMarshal(data interface{}, safeEncoding bool) ([]byte, error) {
 		b, err = json.Marshal(data)
 	}
 
+	// This code is needed if the response contains special characters like &, <, >, 
+	// and those characters must not be converted to safe encoding.
 	if safeEncoding {
 		b = bytes.Replace(b, []byte("\\u003c"), []byte("<"), -1)
 		b = bytes.Replace(b, []byte("\\u003e"), []byte(">"), -1)
