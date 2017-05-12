9431c114b6fc69016fc75f5e784f2b27f0074989
bertt <btemme@hotmai.com>
2017-05-12 09:18:41 +0200

    sorting testing

--- src/http/endpoint_test.go
+++ src/http/endpoint_test.go
@@ -2,9 +2,9 @@ package http
 
 import (
 	"github.com/geodan/gost/src/sensorthings/models"
-	"github.com/geodan/gost/src/sensorthings/rest"
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"sort"
 	"testing"
 )
 
@@ -26,32 +26,80 @@ func TestEndPointLength(t *testing.T) {
 func HandleTest(w http.ResponseWriter, r *http.Request, endpoint *models.Endpoint, api *models.API) {
 }
 
-func TestIsDynamic(t *testing.T){
-	url := "http://www.nu.nl"
-	result := isDynamic(url)
-	assert.False(t, result)
+func TestIsDynamic(t *testing.T) {
+	// arrange
+	urlDynamic := "http://www.{}.nl"
+	urlNotDynamic := "http://www.nu.nl"
+
+	// act
+	resultNotDynamic := isDynamic(urlNotDynamic)
+	resultDynamic := isDynamic(urlDynamic)
+
+	// assert
+	assert.False(t, resultNotDynamic)
+	assert.True(t, resultDynamic)
 }
 
 func TestEndPointSort(t *testing.T) {
 	// arrange
-	restep := rest.Endpoint{}
-	restep.Name = "ep1"
-	// error in next line, why? About models.EndpointOperation
-	//op1 := models.EndpointOperation{models.HTTPOperationGet, "/v1.0/observedproperties",HandleTest}
-	//httpep1 := &Endpoint{Endpoint: restep, Operation: op1}
 	httpep1 := &Endpoint{}
+	httpep1.Operation.Path = "ep1"
+	httpep1.Operation.OperationType = models.HTTPOperationGet
 	httpep2 := &Endpoint{}
+	httpep2.Operation.Path = "ep2"
+	httpep2.Operation.OperationType = models.HTTPOperationPost
 
-	eps := Endpoints{}
-
-	eps = append(eps, httpep1)
-	eps = append(eps, httpep2)
+	eps := Endpoints{httpep1,httpep2}
 
 	// act
-	eps.Swap(0, 1)
-
-	// todo: add some check that the swap is ok
+	sort.Sort(eps)
 
 	// assert
 	assert.True(t, len(eps) == 2, "Number of Endpoints should be 2")
+	// post becomes first after sorting
+	assert.True(t, eps[0].Operation.Path=="ep2")
+	assert.True(t, eps[1].Operation.Path=="ep1")
+}
+
+
+func TestEndPointSortDynamic(t *testing.T) {
+	// arrange
+	httpep1 := &Endpoint{}
+	httpep1.Operation.Path = "ep1{}"
+	httpep1.Operation.OperationType = models.HTTPOperationGet
+	httpep2 := &Endpoint{}
+	httpep2.Operation.Path = "ep2{}longer"
+	httpep2.Operation.OperationType = models.HTTPOperationPost
+
+	eps := Endpoints{httpep1,httpep2}
+
+	// act
+	sort.Sort(eps)
+
+	// assert
+	assert.True(t, len(eps) == 2, "Number of Endpoints should be 2")
+	// when both urls are dynamic, the longer path comes first
+	assert.True(t, eps[0].Operation.Path=="ep2{}longer")
+	assert.True(t, eps[1].Operation.Path=="ep1{}")
+}
+
+func TestEndPointSortlength(t *testing.T) {
+	// arrange
+	httpep1 := &Endpoint{}
+	httpep1.Operation.Path = "ep1"
+	httpep1.Operation.OperationType = models.HTTPOperationGet
+	httpep2 := &Endpoint{}
+	httpep2.Operation.Path = "ep2longer"
+	httpep2.Operation.OperationType = models.HTTPOperationPost
+
+	eps := Endpoints{httpep1,httpep2}
+
+	// act
+	sort.Sort(eps)
+
+	// assert
+	assert.True(t, len(eps) == 2, "Number of Endpoints should be 2")
+	// when both urls are dynamic, the longer path comes first
+	assert.True(t, eps[0].Operation.Path=="ep2longer")
+	assert.True(t, eps[1].Operation.Path=="ep1")
 }
