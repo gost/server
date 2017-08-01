8f9df557177f1a6b93505753964f75434b921319
time <ebben.tim@gmail.com>
2017-08-01 15:29:45 +0200

    Added put handler tests

--- sensorthings/rest/handlers/method_put_test.go
+++ sensorthings/rest/handlers/method_put_test.go
@@ -6,17 +6,23 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"bytes"
+	"errors"
 )
 
 func testHandler() (*entities.Thing, []error) {
 	return nil, nil
 }
 
+func testHandlerError() (*entities.Thing, []error) {
+	return nil, []error {errors.New("Test error")}
+}
+
 func TestHandlePutTestWithWrongDataType(t *testing.T) {
 	// arrange
 	rr := httptest.NewRecorder()
 	thing := &entities.Thing{}
-	req, _ := http.NewRequest("GET", "/bla", nil)
+	req, _ := http.NewRequest("PUT", "/bla", nil)
 	req.Header.Set("Content-Type", "this is an invalid content-type")
 	handle := func() (interface{}, []error) { return testHandler() }
 
@@ -24,19 +30,65 @@ func TestHandlePutTestWithWrongDataType(t *testing.T) {
 	handlePutRequest(rr, nil, req, thing, &handle, false)
 
 	// assert
-	assert.True(t, rr.Code == http.StatusBadRequest)
+	assert.Equal(t,  http.StatusBadRequest, rr.Code)
 }
 
 func TestHandlePutTestWithNoBody(t *testing.T) {
 	// arrange
 	rr := httptest.NewRecorder()
 	thing := &entities.Thing{}
-	req, _ := http.NewRequest("GET", "/bla", nil)
+	req, _ := http.NewRequest("PUT", "/bla", nil)
+	req.Header.Set("Content-Type", "application/json")
 	handle := func() (interface{}, []error) { return testHandler() }
 
 	// act
 	handlePutRequest(rr, nil, req, thing, &handle, false)
 
 	// assert
-	assert.True(t, rr.Code == http.StatusBadRequest)
+	assert.Equal(t,  http.StatusBadRequest, rr.Code)
+}
+
+func TestHandlePutTestWithWrongBody(t *testing.T) {
+	// arrange
+	rr := httptest.NewRecorder()
+	thing := &entities.Thing{}
+	req, _ := http.NewRequest("PUT", "/bla", bytes.NewReader([]byte("{\"name\": 10}")))
+	req.Header.Set("Content-Type", "application/json")
+	handle := func() (interface{}, []error) { return testHandler() }
+
+	// act
+	handlePutRequest(rr, nil, req, thing, &handle, false)
+
+	// assert
+	assert.Equal(t,  http.StatusBadRequest, rr.Code)
+}
+
+func TestHandlePutTestWithPutError(t *testing.T) {
+	// arrange
+	rr := httptest.NewRecorder()
+	thing := &entities.Thing{}
+	req, _ := http.NewRequest("PUT", "/bla", bytes.NewReader([]byte("{\"name\": \"thing1\", \"description\": \"test thing 1\"}")))
+	req.Header.Set("Content-Type", "application/json")
+	handle := func() (interface{}, []error) { return testHandlerError() }
+
+	// act
+	handlePutRequest(rr, nil, req, thing, &handle, false)
+
+	// assert
+	assert.Equal(t,  http.StatusInternalServerError, rr.Code)
+}
+
+func TestHandlePutTestWithGoodBody(t *testing.T) {
+	// arrange
+	rr := httptest.NewRecorder()
+	thing := &entities.Thing{}
+	req, _ := http.NewRequest("PUT", "/bla", bytes.NewReader([]byte("{\"name\": \"thing1\", \"description\": \"test thing 1\"}")))
+	req.Header.Set("Content-Type", "application/json")
+	handle := func() (interface{}, []error) { return testHandler() }
+
+	// act
+	handlePutRequest(rr, nil, req, thing, &handle, false)
+
+	// assert
+	assert.Equal(t,  http.StatusOK, rr.Code)
 }
