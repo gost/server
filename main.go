041a702a743f564daacecddce36980f4a8518c0f
The Zelus <thezelus@gmail.com>
2017-08-20 00:28:47 -0600

    Scaffolding for using logrus

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
+
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/Sirupsen/logrus"
 
 	"github.com/gost/server/configuration"
 	"github.com/gost/server/database/postgis"
@@ -9,15 +16,15 @@ import (
 	"github.com/gost/server/mqtt"
 	"github.com/gost/server/sensorthings/api"
 	"github.com/gost/server/sensorthings/models"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
-var stAPI models.API
-var gostServer http.Server
-var mqttClient models.MQTTClient
+var (
+	stAPI      models.API
+	gostServer http.Server
+	mqttClient models.MQTTClient
+	file       *os.File
+	gostLogger = log.New()
+)
 
 func main() {
 	stop := make(chan os.Signal, 2)
@@ -44,8 +51,21 @@ func main() {
 	}
 
 	configuration.SetEnvironmentVariables(&conf)
-
-	database := postgis.NewDatabase(conf.Database.Host, conf.Database.Port, conf.Database.User, conf.Database.Password, conf.Database.Database, conf.Database.Schema, conf.Database.SSL, conf.Database.MaxIdleConns, conf.Database.MaxOpenConns, conf.Server.MaxEntityResponse)
+	err = initializeLogger(file, conf.Logger.FileName, gostLogger, new(log.TextFormatter), conf.Logger.Verbose)
+	if err != nil {
+		fmt.Println("Error initializing logger, defaulting to stdout. Error: " + err.Error())
+	}
+	database := postgis.NewDatabase(
+		conf.Database.Host,
+		conf.Database.Port,
+		conf.Database.User,
+		conf.Database.Password,
+		conf.Database.Database,
+		conf.Database.Schema,
+		conf.Database.SSL,
+		conf.Database.MaxIdleConns,
+		conf.Database.MaxOpenConns,
+		conf.Server.MaxEntityResponse)
 	go database.Start()
 
 	// if install is supplied create database and close, if not start server
@@ -81,7 +101,13 @@ func createAndStartServer(api *models.API) {
 	a.Start()
 
 	config := a.GetConfig()
-	gostServer = http.CreateServer(config.Server.Host, config.Server.Port, api, config.Server.HTTPS, config.Server.HTTPSCert, config.Server.HTTPSKey)
+	gostServer = http.CreateServer(
+		config.Server.Host,
+		config.Server.Port,
+		api,
+		config.Server.HTTPS,
+		config.Server.HTTPSCert,
+		config.Server.HTTPSKey)
 	gostServer.Start()
 }
 
@@ -89,4 +115,29 @@ func cleanup() {
 	if gostServer != nil {
 		gostServer.Stop()
 	}
+
+	file.Close()
+}
+
+func initializeLogger(file *os.File, logFileName string, logger *log.Logger, format log.Formatter, verboseFlag bool) error {
+
+	file, err := os.OpenFile(logFileName+".log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Log file cannot be opened")
+		file.Close()
+		file = os.Stdout
+	}
+
+	logger.Out = file
+	if format != nil {
+		logger.Formatter = format
+	}
+
+	if verboseFlag {
+		logger.Level = log.DebugLevel
+	} else {
+		logger.Level = log.WarnLevel
+	}
+
+	return err
 }
